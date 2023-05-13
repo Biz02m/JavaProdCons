eff210a911f5a828a977ba3f0892dd84e7780a98
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-05-13 16:12:44 +0200

    Renamed ProductList and added Main

---
+++ src/main/java/ProductsList.java
@@ -0,0 +1,3 @@
+public class ProductsList {
+    public static final String[] products = {"Graphics Card", "High perfomance processor", "RAM", "Motherboard", "SSD", "HDD", "Sound Card", "Wifi card"};
+}
